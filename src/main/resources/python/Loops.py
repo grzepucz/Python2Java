ffb0bdf6f7ba3c29e4629f75264d73bc514ab2cb
grzepucz <grzechu.pe@gmail.com>
2019-06-12 23:53:30 +0200

    resources

--- src/main/resources/python/Loops.py
+++ src/main/resources/python/Loops.py
@@ -6,7 +6,14 @@ for fruit in 'fruits':
     print ('Current fruit :', fruit, ' asas', fruit)
 
 for letter in ["one", "two", "three"]:
-    print ('Current fruit :', letter, ' asas', letter)
+    print ('Current  fruit :', letter, ' asas', letter)
 
-var = 20 - 10
-if ( var == 10 ) : print("Value of expression is 100")
+anda = 20 - 10
+
+if var3 or var1 :
+    print("Value of expression is 100")
+elif (var and var2 or var4):
+    print("Value of expression is 200")
+else:
+    print("Value of expression is 300")
+
